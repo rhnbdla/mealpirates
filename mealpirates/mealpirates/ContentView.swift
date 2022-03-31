e288250a26267e0006b384526bdb7cafee26f918
Rohan Bodla <rbodla3@gatech.edu>
2022-03-31 01:38:22 -0400

    starter code

---
+++ mealpirates/mealpirates/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mealpirates
+//
+//  Created by Rohan Bodla on 3/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mealpirates/mealpirates/mealpiratesApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealpiratesApp.swift
+//  mealpirates
+//
+//  Created by Rohan Bodla on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct mealpiratesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
